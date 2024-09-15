ab4faeeb7c35a954ce247a6f6c233217457e3a4a
colinquinto <165630801+colinquinto@users.noreply.github.com>
2024-09-16 00:12:26 +0800

    Add async function that will use the fetch API to get weather and image data from visual crossing and giphy API

--- script.js
+++ script.js
@@ -15,3 +15,42 @@ const handleSearch = (e) => {
     }
    
 }
+
+const fetchWeather = async (location) => {
+    const loader = document.querySelector('.loader');
+    const errorMsg = document.querySelector('.error');
+    const weatherDiv = document.querySelector('.weather-data');
+    let today = new Date().toLocaleDateString('en-CA');
+    try {
+        weatherDiv.style.visibility = 'hidden';
+        errorMsg.style.display = 'none';
+        errorMsg.textContent = '';
+        loader.style.display = 'block';
+        const getData = await fetch('https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/'
+             + location 
+             + '/' 
+             + today 
+             + '?&unitGroup=uk&key=RGFUEVCF7LUPHF2LEWE66V8M4', {mode: 'cors'});
+        const response = await getData.json();
+        const getGif = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=RBmGbryfHqdHncyzsXd0jEg4sAPPwHe6&s=' 
+            + response.days[0].icon
+            + ' weather&weirdness=1', {mode: 'cors'});
+        const storeGif = await getGif.json();
+        loader.style.display = 'none';
+        weatherDiv.textContent = '';
+        weatherDiv.style.visibility = '';
+        displayData(weatherDiv, response, storeGif);
+        convertEvent();
+    } catch (err){
+        if (err instanceof SyntaxError) {
+            loader.style.display = 'none';
+            errorMsg.style.display = 'block';
+            errorMsg.textContent = "LOCATION NOT FOUND";
+        } else {
+            loader.style.display = 'none';
+            errorMsg.style.display = 'block';
+            errorMsg.textContent = err;
+        }
+        
+    }
+}
